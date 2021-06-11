4355aee8aa5827f38ad69238a35cf9999473d02a
James Bowes <jbowes@repl.ca>
2021-06-11 13:57:38 -0300

    Add package doc file

---
+++ doc.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 James Bowes. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package httpsig provides support for signing and verifying
+HTTP requests according to the Signing HTTP Messages draft standard
+https://datatracker.ietf.org/doc/draft-ietf-httpbis-message-signatures/
+*/
+package httpsig
